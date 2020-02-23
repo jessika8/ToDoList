cbf89a88a146b030e35a352035324e3e4e8a52de
unknown <jessika.roots8@gmail.com>
2020-02-23 11:01:58 +0000

    Updates

---
+++ precache-manifest.5701b8690d77ad6d2ca077c1bc40fba9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43beed74a557ed588558da66b5fb1fa0",
+    "url": "/ToDoList/index.html"
+  },
+  {
+    "revision": "75bfbac30b1db79bc74c",
+    "url": "/ToDoList/static/css/main.975eff99.chunk.css"
+  },
+  {
+    "revision": "d88edfc145ab6e88980e",
+    "url": "/ToDoList/static/js/2.e17b7f37.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/ToDoList/static/js/2.e17b7f37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75bfbac30b1db79bc74c",
+    "url": "/ToDoList/static/js/main.14317f45.chunk.js"
+  },
+  {
+    "revision": "def5cac455aa7ce9b674",
+    "url": "/ToDoList/static/js/runtime-main.e18e039a.js"
+  }
+]);
